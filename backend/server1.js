a9edc159e65256cef539f65c7fd6e8cbcecf5b66
ashvik538 <jashvik176@gmail.com>
2026-06-01 17:57:14 +0530

    king

--- backend/server1.js
+++ backend/server1.js
@@ -5,6 +5,15 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/',(req,res)=>{
+  res.json({
+    status:"Contact API is running",
+    endpoints:{
+      POST:'/api/contact'
+      GET:'/api/messages'
+    }
+  })
+})
 mongoose.connect( process.env.MONGO_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
